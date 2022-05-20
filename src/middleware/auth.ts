db9d742c5e1bbcfad09876aea0b7e05c06dc25ea
izzudin26 <izzudin.ar26@gmail.com>
2022-05-21 03:37:02 +0700

    add middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,20 @@
+import { FastifyRequest } from 'fastify'
+import jwt from 'jsonwebtoken'
+import { httpResponse } from '../types'
+
+export const jwtAuth = async (req: FastifyRequest, res: any, next: any) => {
+  const token = req.headers.authorization
+
+  try {
+    if (!token) {
+      httpResponse(401, 'Token is empty')
+    }
+    const verify = await jwt.verify(token!, 'barchive')
+    if (!verify) {
+      httpResponse(403, 'Wrong token')
+    }
+    next()
+  } catch (error) {
+    if (error instanceof Error) throw httpResponse(500, error.message)
+  }
+}
