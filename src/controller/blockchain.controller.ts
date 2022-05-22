f01f586e55310b79130be00e4fa5491adddd5ab6
izzudin26 <izzudin.ar26@gmail.com>
2022-05-22 11:04:02 +0700

    add route insert blockchain data

--- src/controller/blockchain.controller.ts
+++ src/controller/blockchain.controller.ts
@@ -1,7 +1,17 @@
 import { FastifyRequest } from 'fastify'
+import { ITransaction } from '../model/transaction'
 import { broadcastData, getBlockdata } from '../services/nodeblockchain.service'
-import { httpResponse } from '../types'
+import { httpResponse, IPayloadUser } from '../types'
+import jwt from 'jsonwebtoken'
 
 export const uploadImage = async (req: any, res: any) => {
   return httpResponse(200, 'Success', { imageName: req.file.filename })
+}
+
+export const insertData = async (req: FastifyRequest<{Body: ITransaction}>, res: any) => {
+  const token = req.headers.authorization as string
+  const data: ITransaction = req.body
+  const decodeToken = jwt.verify(token, 'barchive') as IPayloadUser
+  await broadcastData(decodeToken._id, data)
+  return httpResponse(200, 'Data has been stored', data)
 }
--- src/router/blockchain.route.ts
+++ src/router/blockchain.route.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { upload } from '../server'
 import * as blockchainController from '../controller/blockchain.controller'
+import { jwtAuth } from '../middleware/auth'
 
 export default async (fastify: FastifyInstance, opts: any) => {
   fastify.route({
@@ -8,5 +9,12 @@ export default async (fastify: FastifyInstance, opts: any) => {
     url: '/image',
     preHandler: upload.single('image'),
     handler: blockchainController.uploadImage
+  })
+
+  fastify.route({
+    method: 'POST',
+    url: '/',
+    preHandler: jwtAuth,
+    handler: blockchainController.insertData
   })
 }
